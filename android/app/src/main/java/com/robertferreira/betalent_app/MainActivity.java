276d5cc26e1e81bb5c82c43fa940cf29a81145b1
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2024-11-23 08:53:12 -0300

    chore: configure platforms support

--- android/app/src/main/java/com/robertferreira/betalent_app/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.robertferreira.betalent_app;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/robertferreira/betalent_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.robertferreira.betalent_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
