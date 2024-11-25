79f459fe4b1fb30ea14030f06c1e603a36ca3fe5
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2024-11-25 20:38:50 -0300

    feat(splash): Implemented the splash page

---
+++ lib/src/splash/pages/splash_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 1)).then((_) => navigationHome());
+  }
+
+  void navigationHome() => Navigator.of(context).pushReplacementNamed('/home');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF0500FF),
+      body: Center(
+        child: Image.asset(
+          'assets/images/logo.png',
+          fit: BoxFit.cover,
+          height: 39,
+          width: 198.31,
+        ),
+      ),
+    );
+  }
+}
