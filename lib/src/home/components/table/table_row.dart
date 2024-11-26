44aac3f2a3a8793f5c7920ea3cf1d3e75da7d8c6
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2024-11-25 22:13:42 -0300

    feat(table row): Implemented a dotted bottom line for the employee details textr

--- lib/src/home/components/table/table_row.dart
+++ lib/src/home/components/table/table_row.dart
@@ -1,6 +1,7 @@
 import 'package:betalent_app/src/extensions/date_extensions.dart';
 import 'package:betalent_app/src/extensions/string_extensions.dart';
 import 'package:betalent_app/src/home/models/employee_model.dart';
+import 'package:dotted_line/dotted_line.dart';
 import 'package:flutter/material.dart';
 
 class TableRowComponent extends StatefulWidget {
@@ -45,79 +46,102 @@ class _TableRowComponentState extends State<TableRowComponent> {
         const SizedBox(height: 31.5),
         Padding(
           padding: const EdgeInsets.only(left: 16, right: 23),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: [
-              const Text(
-                'Cargo',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontFamily: 'HelveticaNeue',
-                  fontWeight: FontWeight.w500,
-                  color: Color(0xFF1C1C1C),
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Cargo',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'HelveticaNeue',
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF1C1C1C),
+                    ),
+                  ),
+                  Text(
+                    employee.job,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'HelveticaNeue',
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF1C1C1C),
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                employee.job,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontFamily: 'HelveticaNeue',
-                  fontWeight: FontWeight.w400,
-                  color: Color(0xFF1C1C1C),
-                ),
-              ),
+              const DottedLine(
+                dashColor: Color(0xFFDFDFDF),
+              )
             ],
           ),
         ),
+        const SizedBox(height: 15),
         Padding(
           padding: const EdgeInsets.only(left: 16, right: 23),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: [
-              const Text(
-                'Data de admissão',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontFamily: 'HelveticaNeue',
-                  fontWeight: FontWeight.w500,
-                  color: Color(0xFF1C1C1C),
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Data de admissão',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'HelveticaNeue',
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF1C1C1C),
+                    ),
+                  ),
+                  Text(
+                    employee.admissionDate.brl,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'HelveticaNeue',
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF1C1C1C),
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                employee.admissionDate.brl,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontFamily: 'HelveticaNeue',
-                  fontWeight: FontWeight.w400,
-                  color: Color(0xFF1C1C1C),
-                ),
-              ),
+              const DottedLine(
+                dashColor: Color(0xFFDFDFDF),
+              )
             ],
           ),
         ),
+        const SizedBox(height: 15),
         Padding(
           padding: const EdgeInsets.only(left: 16, right: 23),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
             children: [
-              const Text(
-                'Telefone',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontFamily: 'HelveticaNeue',
-                  fontWeight: FontWeight.w500,
-                  color: Color(0xFF1C1C1C),
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Telefone',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'HelveticaNeue',
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF1C1C1C),
+                    ),
+                  ),
+                  Text(
+                    employee.phoneNumber.phoneNumberBrl,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'HelveticaNeue',
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF1C1C1C),
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                employee.phoneNumber.phoneNumberBrl,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontFamily: 'HelveticaNeue',
-                  fontWeight: FontWeight.w400,
-                  color: Color(0xFF1C1C1C),
-                ),
-              ),
+              const DottedLine(
+                dashColor: Color(0xFFDFDFDF),
+              )
             ],
           ),
         ),
